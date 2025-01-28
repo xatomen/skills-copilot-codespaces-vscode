45cb52c7263e81b9bb43b7e8f6385e1748bb8ea4
Jorge Gallardo <64385598+xatomen@users.noreply.github.com>
2025-01-28 13:12:13 +0000

    Copilot third comment

--- comments.js
+++ comments.js
@@ -1,24 +1,21 @@
-// Create a web server
-const http = require('http');
-const fs = require('fs');
+// Create a web server that listens on port 3000. When you visit the home page, the server should respond with "Hello, World!" When you visit localhost:3000/comments, the server should respond with an array of comments (as strings). The array should have at least 3 comments.
 
-// Create a server object
-http.createServer((req, res) => {
-  // If the URL is /, read the index.html file
-  if (req.url === '/') {
-    fs.readFile('index.html', (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    });
-  }
+const express = require('express');
+const app = express();
 
-  // If the URL is /comments, read the comments.json file
-  if (req.url === '/comments') {
-    fs.readFile('comments.json', (err, data) => {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(data);
-      return res.end();
-    });
-  }
-}).listen(8080); // Server object listens on port 8080
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/comments', (req, res) => {
+  let comments = [
+    'This is the first comment',
+    'This is the second comment',
+    'This is the third comment'
+  ];
+  res.send(comments);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
